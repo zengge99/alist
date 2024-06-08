6ef0e80c86068547b24cc48f40e6bf53ccde8a96
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-06-08 13:47:20 +0800

    Update types.go

--- drivers/base/types.go
+++ drivers/base/types.go
@@ -7,9 +7,10 @@ type Json map[string]interface{}
 type TokenResp struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
-	//zzzzzzzzzzzzzzzzzzz
-	AliOpenRefreshToken string
-	AliOpenAccessToken string
 }
 
 type ReqCallback func(req *resty.Request)
+
+//zzzzzzzzzzzzzzzzzzz
+AliOpenRefreshToken string = ""
+AliOpenAccessToken string = ""
