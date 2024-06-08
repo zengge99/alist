7fb71d17e65a11fea2d4b3eee9472eebc9fad748
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-06-09 00:20:05 +0800

    Update meta.go

--- drivers/aliopen_share/meta.go
+++ drivers/aliopen_share/meta.go
@@ -21,6 +21,7 @@ type Addition struct {
 	
 	//115参数
 	Cookie       string  `json:"cookie" type:"text" help:"115 cookie required"`
+	DirId       string  `json:"dir_id" type:"text" help:"115 temp dir id"`
 	PageSize     int64   `json:"page_size" type:"number" default:"56" help:"list api per page size of 115 driver"`
 	LimitRate    float64 `json:"limit_rate" type:"number" default:"2" help:"limit all api request rate (1r/[limit_rate]s)"`
 }
