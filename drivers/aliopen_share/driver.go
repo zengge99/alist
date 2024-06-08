9d63025401ee41a38785d22f8d1ff819780509c2
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-06-09 03:57:16 +0800

    Update driver.go

--- drivers/aliopen_share/driver.go
+++ drivers/aliopen_share/driver.go
@@ -292,7 +292,7 @@ func (d *AliyundriveShare2Open) Link(ctx context.Context, file model.Obj, args m
 
 	fmt.Println("115下载链接：", downloadInfo.Url.Url)
 
-	d.FileID_Link_model[file_id] = link;
+	d.FileID_Link_model[file_id] = link
 	
 	return link, nil
 }
