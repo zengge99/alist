ea9130aeb8f01a758f3fd1f12c16c31b4b578e31
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-06-08 11:46:06 +0800

    Update driver.go

--- drivers/aliopen_share/driver.go
+++ drivers/aliopen_share/driver.go
@@ -167,7 +167,7 @@ func (d *AliyundriveShare2Open) Link(ctx context.Context, file model.Obj, args m
 			Header: http.Header{
 				"Referer": []string{"https://www.aliyundrive.com/"},
 			},
-			URL: "http://img.xiaoya.pro/abnormal.png",
+			URL: "http:/GetmyLink/img.xiaoya.pro/abnormal.png",
 		}, nil
 	} 
 
@@ -221,6 +221,9 @@ func (d *AliyundriveShare2Open) Copy2Myali(ctx context.Context, src_driveid stri
 		return "", err
 	}
 	
+	//zzzzzzzzzzzzzzzzzzzzz
+	fmt.Println("转存原始响应: ",string(r))
+
 	var responses map[string]interface{}
 	json.Unmarshal([]byte(r), &responses)
 
