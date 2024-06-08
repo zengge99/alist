1dbeb375bf8433171b238ee7b252f8c07f97d518
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-06-08 16:04:44 +0800

    Update all.go

--- drivers/all.go
+++ drivers/all.go
@@ -57,6 +57,7 @@ import (
 	_ "github.com/alist-org/alist/v3/drivers/weiyun"
 	_ "github.com/alist-org/alist/v3/drivers/wopan"
 	_ "github.com/alist-org/alist/v3/drivers/yandex_disk"
+	_ "github.com/alist-org/alist/v3/drivers/aliopen_share"
 )
 
 // All do nothing,just for import
