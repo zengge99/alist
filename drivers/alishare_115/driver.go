ad83be45777cc9c8ce9c21787414acaceba121e6
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-06-09 08:20:49 +0800

    Update driver.go

--- drivers/alishare_115/driver.go
+++ drivers/alishare_115/driver.go
@@ -292,7 +292,7 @@ func (d *AliyundriveShare2Pan115) Link(ctx context.Context, file model.Obj, args
 
 	d.FileID_Link_model[file_id] = link
 
-	if files, ok := d.client.List(d.dirID); ok{
+	if files, ok := d.client.List(d.DirId); ok{
 		for index, value := range files {
 			if value.Name == ss.GetName() && strings.ToUpper(value.Sha1) == fullHash{
 				d.client.Delete(value.FileID)
