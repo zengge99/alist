42c3b3f979af5cbf19c1fcb4cb8fa386dd8197de
zengge99 <83830807+zengge99@users.noreply.github.com>
2024-06-12 11:42:27 +0800

    Update driver.go

--- drivers/alishare_115/driver.go
+++ drivers/alishare_115/driver.go
@@ -280,10 +280,7 @@ func (d *AliyundriveShare2Pan115) Link(ctx context.Context, file model.Obj, args
 		return link, nil
 	}
 
-	preHash, err := calculateSHA1Range(link.URL, 0, 128*1024-1)
-	if err != nil {
-		return link, nil
-	}
+	preHash := "2EF7BDE608CE5404E97D5F042F95F89F1C232871"
 	fullHash := ContentHash
 
 	var fastInfo *driver115.UploadInitResp
