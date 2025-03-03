56f84469329c83790d49b3a4312000b49ff53966
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-03-03 17:24:55 +0800

    Update types.go

--- drivers/alishare_115/types.go
+++ drivers/alishare_115/types.go
@@ -2,8 +2,6 @@ package alishare_pan115
 
 import (
 	"time"
-	"github.com/SheltonZhu/115driver/pkg/driver"
-	"github.com/alist-org/alist/v3/pkg/utils"
 	"github.com/alist-org/alist/v3/internal/model"
 )
 
@@ -60,18 +58,4 @@ type ShareLinkResp struct {
 type OpenResp struct {
 	DriverID string `json:"drive_id"`
 	FileID   string `json:"file_id"`
-}
-
-var _ model.Obj = (*FileObj)(nil)
-
-type FileObj struct {
-	driver.File
-}
-
-func (f *FileObj) CreateTime() time.Time {
-	return f.File.CreateTime
-}
-
-func (f *FileObj) GetHash() utils.HashInfo {
-	return utils.NewHashInfo(utils.SHA1, f.Sha1)
 }
